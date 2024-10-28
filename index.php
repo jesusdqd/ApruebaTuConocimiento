f830a88829db3f3f69f7209529103067615b0281
jesusdqd <jesusquenoran2016@gmail.com>
2024-10-28 17:08:09 -0500

    Inicio

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aprueba tu conocimiento</title>
+    <link rel="stylesheet" href="Css/estilos.css">
+    <link rel="stylesheet" href="Css/barra_navegacion.css">
+    <link rel="stylesheet" href="Css/areas.css">
+    <style>
+        body {
+            cursor: url('Assets/Cursor.cur'), auto;  
+            min-height: 100vh;
+            min-width: 100vh;
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+    <?php include('Html/navbar.html'); ?>
+    <?php include('Html/areas.html'); ?>    
+</body>
